9da7dfe73f3aff0e08a3154e23b81331db878d19
Ariph Ansari <143442610+ariphmohd@users.noreply.github.com>
2025-05-17 16:53:44 +0530

    Update 3 main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3-bucket-setup" {
-    bucket = "my-unique-bucket-name-123456789"
+    bucket = "my-unique-bucket-name-12345678923"
 
     tags = {
         Name = "my-s3-bucket"
