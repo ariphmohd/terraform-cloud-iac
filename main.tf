00865330bf05542702fdc19695ef1289a2c24316
Ariph Ansari <143442610+ariphmohd@users.noreply.github.com>
2025-05-17 16:37:52 +0530

    Update storageaccount name.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucketviaHCLCloud" {
-  bucket = "my-tf-test-bucket${random_string.bucket_suffix.result}"
+  bucket = "my-tf-test-bucket${random_id.bucket.hex}"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
